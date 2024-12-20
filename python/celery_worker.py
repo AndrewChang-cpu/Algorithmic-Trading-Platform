d6b7721bbe457a120e487465f0469b45d4e7e494
CandymanAndy <74220837+AndrewChang-cpu@users.noreply.github.com>
2024-12-19 22:08:33 -0600

    Properly configured celery

---
+++ python/celery_worker.py
@@ -0,0 +1,37 @@
+from celery import Celery
+import backtrader as bt
+from strategy import TestStrategy
+import sys
+
+######################
+# This seems to be necessary to run Celery. Remove if not needed.
+class FlushFile:
+    def __init__(self, f):
+        self.f = f
+
+    def write(self, x):
+        self.f.write(x)
+        self.f.flush()
+
+    def flush(self):
+        self.f.flush()
+
+    def isatty(self):
+        return False
+
+sys.stdout = FlushFile(sys.stdout)
+sys.stderr = FlushFile(sys.stderr)
+######################
+
+app = Celery('tasks', broker='redis://localhost:6379/0')
+
+@app.task
+def run_test_strategy(stake):
+    # Configure and run the TestStrategy instance
+    cerebro = bt.Cerebro()
+    cerebro.addstrategy(TestStrategy, stake=stake)
+    cerebro.run()
+    return "Strategy executed with stake: {}".format(stake)
+
+# Usage
+# - curl -X POST http://localhost:8080/publish_task -H "Content-Type: application/json" -d '{"stake": 10}'
