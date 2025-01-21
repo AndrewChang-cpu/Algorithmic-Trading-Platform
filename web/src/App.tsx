e319975bdb0042f52e0e58a36371bf2a261bdae1
CandymanAndy <74220837+AndrewChang-cpu@users.noreply.github.com>
2025-01-20 21:16:14 -0600

    Formatted multiple Charts on main page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import Chart from './components/Chart';
+import './App.css';
 
 const App: React.FC = () => {
   return (
     <div>
       <h1>Portfolio Value Chart</h1>
-      <Chart userId='TODO' />
+      <div className="chart-grid">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <div key={index} className="chart-container">
+            <Chart userId="TODO" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
